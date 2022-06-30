ce95362c361e485ec13045956d448a704c0543a1
the-Fish2 <97804639+the-Fish2@users.noreply.github.com>
2022-06-30 12:57:09 -0700

    Update code.py

--- code.py
+++ code.py
@@ -27,5 +27,6 @@ while curr_events == RISING:
     time.sleep(20)
     
 #results should have values: 'particles 40um', 'particles 10um', 'pm10 standard', 'pm100 standard', 'pm25 standard', 'particles 25um', 'particles 100um', 'particles 05um', 'tps', 'pm40 standard'
+#note that pm100 standard and pm40 standard ARE GUESSES BASED ON SURROUNDINGS, only exact data is 25 and 10
 # other sensors running
 #remember - indentation necessary in python (ew)
