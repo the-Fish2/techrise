97b2a92736f2e48322019df82eb30358c9053084
the-Fish2 <97804639+the-Fish2@users.noreply.github.com>
2022-06-30 18:10:06 -0700

    Update code.py

--- code.py
+++ code.py
@@ -5,6 +5,7 @@ import sdcardio
 import storage
 from adafruit_sps30.i2c import SPS30_I2C
 import adafruit_bme688
+import adafruit_vc0706
 
 #do not rearrange code; SD card must come first
 spi = board.SPI()
@@ -15,11 +16,18 @@ storage.mount(vfs, "/sd")
 
 i2c = board.I2C()
 pmsensor = SPS30_I2C(i2c, 0x69)
-aqsensor = adafruit_bme688.Adafruit_BME680_I2C(i2c, 0x77) #if this doesn't work try 0x76
 
+aqsensor = adafruit_bme688.Adafruit_BME680_I2C(i2c, 0x77) #if this doesn't work try 0x76
 aqsensor.sea_level_pressure = 1013
 temperature_offset = -5 
 #needs to be calibrated
+
+uart = busio.UART(board.TX, board.RX) #i think just d0 d1 will check later
+vc0706 = adafruit_vc0706.VC0706(uart)
+vc0706.baudrate = 115200
+vc0706.image_size = adafruit_vc0706.IMAGE_SIZE_320x240
+frame_length = vc0706.frame_length
+initAlt = 0;
 
 while curr_events == RISING:
     
@@ -29,7 +37,8 @@ while curr_events == RISING:
     
     with open("/sd/readings.txt", "a") as f:
         #balloon
-        f.write('ALTITUDE', '%.3f' % TRsim.altitude)
+        currAlt = TRsim.altitude;
+        f.write('ALTITUDE', '%.3f' % currAlt)
         
         #aqi
         temp = aqsensor.temperature();
@@ -38,6 +47,7 @@ while curr_events == RISING:
         f.write('Humidity: {}%'.format(aqsensor.humidity))
         f.write('Pressure: {}hPa'.format(aqsensor.pressure))
         
+        #ought to test whether cleaning the fan impacts the reading or if not, do so at home
         #cleaning the fan of the pm sensor - code found on https://github.com/kevinjwalters/Adafruit_CircuitPython_SPS30/blob/master/examples/sps30_test.py
         sps30_fp.clean(wait=4)
         for i in range(2 * (10 - 4 + 15)):
@@ -66,6 +76,18 @@ while curr_events == RISING:
         
     pmsensor.stop();
     pmsensor.sleep();
+    
+    if (frame_length > 0 and currAlt - initAlt >= 3500): 
+        with open("/sd/image.jpg", "wb") as f:
+            # Compute how much data is left to read as the lesser of remaining bytes
+            # or the copy buffer size (32 bytes at a time).  Buffer size MUST be
+            # a multiple of 4 and under 100.  Stick with 32!
+            to_read = min(frame_length, 32)
+            copy_buffer = bytearray(to_read)
+            vc0706.read_picture_into(copy_buffer)
+            f.write(copy_buffer)
+            frame_length -= 32
+            initAlt = currAlt
     
     time.sleep(20)
     
